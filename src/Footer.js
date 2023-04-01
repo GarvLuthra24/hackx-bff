1aec2cdacfe8e9475ab0b188ab72e16a66dc35f8
Garv <99045108+GarvLuthra24@users.noreply.github.com>
2023-04-01 12:55:39 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='Footer'> 
+        
+        <div  className=' bg-gray-600 h-96 flex flex-row text-white'>
+            <div className=' flex flex-row  w-3/5 justify-around bg-red-400 ml-auto mr-auto'>
+                <div>ABOUT</div>
+                <div>MENU</div>
+                <div>CONTACT INFO</div>
+                <div>LATEST NEWS</div>
+
+                
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
